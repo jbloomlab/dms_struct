18e453b393c845284cdb2bb458d5d7231bc43606
jbloom <jbloom@fredhutch.org>
2019-04-27 20:56:04 -0700

    added `colorschemes` module

--- setup.py
+++ setup.py
@@ -44,6 +44,10 @@ setup(
     long_description=readme,
     license='GPLv3',
     install_requires=[
+                      'matplotlib>=3.0.2',
+                      'nglview>=1.2',
+                      'numpy>=1.15.4',
+                      'pandas>=0.24.2',
                       ],
     platforms='Linux and Mac OS X.',
     packages=['dms_struct'],
